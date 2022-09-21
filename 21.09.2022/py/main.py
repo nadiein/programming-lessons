d042f0d0430f997de3e6ef98ce514fbc6496f814
Igor <snoozzzy7@gmail.com>
2022-09-21 18:24:50 +0200

    Added task description for py and init func

---
+++ 21.09.2022/py/main.py
@@ -0,0 +1,20 @@
+# Given a number of points on a plane, your task is to find two points
+# with the smallest distance between them in linearithmic O(n log n) time.
+# Example:
+#     1  2  3  4  5  6  7  8  9
+#   1  
+#   2    . A
+#   3                . D
+#   4                   . F       
+#   5             . C
+#   6              
+#   7                . E
+#   8    . B
+#   9                   . G
+
+
+class WrongType(Exception):
+    pass
+
+def findClosestPairs(points):
+    pass
