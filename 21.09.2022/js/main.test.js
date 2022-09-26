af0ab074a3b49bddc0b07670708310ce14aa5fd9
Igor <snoozzzy7@gmail.com>
2022-09-27 01:01:23 +0200

    Fixed tests for js

--- 21.09.2022/js/main.test.js
+++ 21.09.2022/js/main.test.js
@@ -1,5 +1,4 @@
-const findClosestDistance = require('./main');
-const findClosestPair = require('./main');
+import { findClosestDistance, findClosestPair } from './main';
 
 
 const test_array_1 = [ [2,2],[2,8],[5,5],[6,3],[6,7],[7,4],[7,9] ];
@@ -7,25 +6,25 @@ const test_array_2 = [ [2,2],[2,8],[5,5],[5,5],[6,3],[6,7],[7,4],[7,9] ];
 
 
 test('Get float number that represent distance between coordinates from 1, 1, 1, 1 to be equal 0.0', () => {
-    expect(findClosestDistance(1, 1, 1, 1)).toBe(0.0)
+    expect(findClosestDistance(1, 1, 1, 1)).toEqual(0.0)
 });
 
-// test('Get float number that represent distance between coordinates from 9, 6, 2, 8 to be equal 0.0', () => {
-//     expect(findClosestDistance(9, 6, 2, 8)).toBe(7.3)
-// });
+test('Get float number that represent distance between coordinates from 9, 6, 2, 8 to be equal 7.3', () => {
+    expect(findClosestDistance(9, 6, 2, 8)).toBe(7.3)
+});
 
-// test('Get float number that represent distance between coordinates from 5, 5, 5, 5 to be equal 0.0', () => {
-//     expect(findClosestDistance(5, 5, 5, 5)).toBe(0.0)
-// });
+test('Get float number that represent distance between coordinates from 5, 5, 5, 5 to be equal 0.0', () => {
+    expect(findClosestDistance(5, 5, 5, 5)).toBe(0.0)
+});
 
-// test('Get float number that represent distance between coordinates from 6, 3, 7, 4 to be equal 0.0', () => {
-//     expect(findClosestDistance(6, 3, 7, 4)).toBe(1.4)
-// });
+test('Get float number that represent distance between coordinates from 6, 3, 7, 4 to be equal 1.4', () => {
+    expect(findClosestDistance(6, 3, 7, 4)).toBe(1.4)
+});
 
-// test('Get [ [2, 2], [2, 8], [5, 5], [6, 3], [6, 7], [7, 4], [7, 9] ] and return array of closest points [6, 3], [7, 4]', () => {
-//     expect(findClosestPair(test_array_1)).toStrictEqual([[6, 3], [7, 4]])
-// });
+test('Get [ [2, 2], [2, 8], [5, 5], [6, 3], [6, 7], [7, 4], [7, 9] ] and return array of closest points [6, 3], [7, 4]', () => {
+    expect(findClosestPair(test_array_1)).toEqual([[6, 3], [7, 4]])
+});
 
-// test('Get [ [2, 2], [2, 8], [5, 5], [5, 5], [6, 3], [6, 7], [7, 4], [7, 9] ] and return array of closest points [5, 5], [5, 5]', () => {
-//     expect(findClosestPair(test_array_2)).toStrictEqual([[5, 5], [5, 5]])
-// })
+test('Get [ [2, 2], [2, 8], [5, 5], [5, 5], [6, 3], [6, 7], [7, 4], [7, 9] ] and return array of closest points [5, 5], [5, 5]', () => {
+    expect(findClosestPair(test_array_2)).toEqual([[5, 5], [5, 5]])
+})
