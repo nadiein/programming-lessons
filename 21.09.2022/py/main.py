0216d261ced9a9da3f239e4697d07b34937ff188
Igor <snoozzzy7@gmail.com>
2023-01-19 14:34:24 +0100

    🚧 Added to the py additional solutions

--- 21.09.2022/py/main.py
+++ 21.09.2022/py/main.py
@@ -12,7 +12,7 @@
 #   8    . B
 #   9                   . G
 
-import math
+from math import sqrt
 import time
 from timeit import default_timer as timer
 import cProfile
@@ -21,16 +21,17 @@ import cProfile
 class WrongType(Exception):
     pass
 
+# Solution one
 def find_distance(x1:int, y1:int, x2:int, y2:int) -> float:
-    return round(math.sqrt((x2 - x1)**2 + (y2 - y1)**2), 1)
+    return round(sqrt((x2 - x1)**2 + (y2 - y1)**2), 1)
 
-# TODO:// refactor algorithm, it\'s too slow. Should be O(n log n)
-def find_closest_pairs(points:tuple) -> tuple:
+def find_closest_pairs_one(points:tuple) -> tuple:
     if not isinstance(points, tuple):
         raise WrongType
 
     l = list()
     count = 1
+
     for index, point in enumerate(points):
         for sub_index, sub_point in enumerate(points[count:], start=count):
             distance = find_distance(point[0], point[1], sub_point[0], sub_point[1])
@@ -52,8 +53,25 @@ def find_closest_pairs(points:tuple) -> tuple:
 
     return (points[x], points[y])
 
+# Solution two
+def find_closest_pairs_two(points):
+    def distance(p1, p2):
+        return sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
 
-points = (
+    min_distance = float('inf')
+    closest_points = None
+
+    for i in range(len(points)):
+        for j in range(i+1, len(points)):
+            d = distance(points[i], points[j])
+
+            if d < min_distance:
+                min_distance = d
+                closest_points = [points[i], points[j]]
+
+    return closest_points
+
+points_one = (
     (2, 2), # A
     (2, 8), # B
     (5, 5), # C
@@ -63,11 +81,27 @@ points = (
     (7, 9)  # G
 )
 
+points_two = [
+    (2, 2), # A
+    (2, 8), # B
+    (5, 5), # C
+    (6, 3), # D
+    (6, 7), # E
+    (7, 4), # F
+    (7, 9)  # G
+]
+
 
 if __name__ == '__main__':
-    cProfile.run('find_closest_pairs(points)')
+    cProfile.run('find_closest_pairs_one(points_one)')
+    cProfile.run('find_closest_pairs_two(points_two)')
 
     start_time = time.time()
-    find_closest_pairs(points)
+    find_closest_pairs_one(points_one)
+    end_time = time.time()
+    print(end_time - start_time)
+
+    start_time = time.time()
+    find_closest_pairs_two(points_two)
     end_time = time.time()
     print(end_time - start_time)
