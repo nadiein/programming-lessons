d8df3719174b9ce39943e13013c73995c77103d6
Igor Nadiein <snoozzzy7@gmail.com>
2023-01-29 13:03:08 +0100

    🚧 Inited js folder for challenge. Added class with static methods

---
+++ 29.01.2023/js/main.js
@@ -0,0 +1,17 @@
+// Create a RomanNumerals class that can convert a roman numeral to and from an integer value.
+// Modern Roman numerals are written by expressing each digit separately starting with the left
+// most digit and skipping any digit with a value of zero.
+// In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC.
+// 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
+
+
+/* Solution one */
+export class RomanNumerals {
+    static toRoman = (number) => {
+        return '';
+    }
+
+    static fromRoman = (string) => {
+        return 0;
+    }
+}
