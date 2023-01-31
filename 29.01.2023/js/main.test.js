f62587c2ea1fdcdb16a34f47efa27ea6da1b1e64
Igor Nadiein <snoozzzy7@gmail.com>
2023-01-31 05:30:45 +0100

    ✅ Added unit tests for js challenge

--- 29.01.2023/js/main.test.js
+++ 29.01.2023/js/main.test.js
@@ -3,19 +3,19 @@ import { RomanNumerals } from './main';
 
 describe('Test RomanNumerals class that call toRoman method and return roman number', () => {
     const testsToRoman = [
-        [RomanNumerals.toRoman(1000), 'M'],
-        [RomanNumerals.toRoman(4), 'IV'],
-        [RomanNumerals.toRoman(1), 'I'],
-        [RomanNumerals.toRoman(1990), 'MCMXC'],
-        [RomanNumerals.toRoman(2008), 'MMVIII']
+        [1000, 'M'],
+        [4, 'IV'],
+        [1, 'I'],
+        [1990, 'MCMXC'],
+        [2008, 'MMVIII']
     ];
 
     const testsFromRoman = [
-        [RomanNumerals.fromRoman('XXI'), 21],
-        [RomanNumerals.fromRoman('I'), 1],
-        [RomanNumerals.fromRoman('IV'), 4],
-        [RomanNumerals.fromRoman('MMVIII'), 2008],
-        [RomanNumerals.fromRoman('MDCLXVI'), 1666]
+        ['XXI', 21],
+        ['I', 1],
+        ['IV', 4],
+        ['MMVIII', 2008],
+        ['MDCLXVI', 1666]
     ];
 
     it('test to roman method', () => {
@@ -23,6 +23,7 @@ describe('Test RomanNumerals class that call toRoman method and return roman num
             expect(RomanNumerals.toRoman(input)).toBe(expected);
         }
     });
+
     it('test from roman method', () => {
         for ( const [input, expected] of testsFromRoman ) {
             expect(RomanNumerals.fromRoman(input)).toBe(expected);
