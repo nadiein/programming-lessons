4337d86997ea2c1426301992ff63f9662a56886c
Igor Nadiein <snoozzzy7@gmail.com>
2023-01-31 05:36:00 +0100

    ✅ Added unit tests for py solution

--- 29.01.2023/py/test_main.py
+++ 29.01.2023/py/test_main.py
@@ -4,7 +4,24 @@ import main
 # First approach
 def test():
     # First approach
-    cases = []
+    cases_to = (
+        (1000, 'M'),
+        (4, 'IV'),
+        (1, 'I'),
+        (1990, 'MCMXC'),
+        (2008, 'MMVIII')
+    );
 
-    for x, y in cases:
-        assert main._(x) == y
+    cases_from = (
+        ('XXI', 21),
+        ('I', 1),
+        ('IV', 4),
+        ('MMVIII', 2008),
+        ('MDCLXVI', 1666)
+    );
+
+    for x, y in cases_to:
+        assert main.RomanNumerals.toRoman(x) == y
+
+    for x, y in cases_from:
+        assert main.RomanNumerals.fromRoman(x) == y
