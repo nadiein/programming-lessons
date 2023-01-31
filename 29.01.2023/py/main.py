e64cf97c9064ecbcd33328be752f11b2195a630a
Igor Nadiein <snoozzzy7@gmail.com>
2023-01-31 05:35:29 +0100

    🚧 Added py solution of challenge

--- 29.01.2023/py/main.py
+++ 29.01.2023/py/main.py
@@ -9,6 +9,62 @@ class WrongType(Exception):
     pass
 
 # Solution one
+class RomanNumerals:
+
+    @staticmethod
+    def fromRoman(str):
+        map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        result = 0
+        i = 0
+
+        while i < len(str):
+            current = map[str[i]]
+
+            if i + 1 < len(str) and map[str[i + 1]] > current:
+                result += map[str[i + 1]] - current
+                i += 2
+            else:
+                result += current
+                i += 1
+
+        return result
+
+    @staticmethod
+    def toRoman(num):
+        map = [
+            { 'roman': 'M', 'value': 1000 },
+            { 'roman': 'CM', 'value': 900 },
+            { 'roman': 'D', 'value': 500 },
+            { 'roman': 'CD', 'value': 400 },
+            { 'roman': 'C', 'value': 100 },
+            { 'roman': 'XC', 'value': 90 },
+            { 'roman': 'L', 'value': 50 },
+            { 'roman': 'XL', 'value': 40 },
+            { 'roman': 'X', 'value': 10 },
+            { 'roman': 'IX', 'value': 9 },
+            { 'roman': 'V', 'value': 5 },
+            { 'roman': 'IV', 'value': 4 },
+            { 'roman': 'I', 'value': 1 }
+        ]
+
+        result = ''
+
+        for item in map:
+
+            while num >= item['value']:
+                result += item['roman']
+                num -= item['value']
+
+        return result
 
 
 import cProfile
