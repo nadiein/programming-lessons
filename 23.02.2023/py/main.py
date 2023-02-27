ba95f6e679ecd1127587ef817944b2c8e3da0dd1
Igor Nadiein <snoozzzy7@gmail.com>
2023-02-27 13:22:37 -0800

    🚧 Added challenge js description and solution

--- 23.02.2023/py/main.py
+++ 23.02.2023/py/main.py
@@ -1,44 +1,137 @@
-# Calculate fib(n) where:
+# Automatons, or Finite State Machines (FSM), are extremely useful to programmers when it comes to software design. You will be given a simplistic version of an FSM to code for a basic TCP session.
 
-# fib(0) := 0
-# fib(1) := 1
-# fin(n + 2) := fib(n + 1) + fib(n)
-# Write an algorithm that can handle n up to 2000000.
+# The outcome of this exercise will be to return the correct state of the TCP FSM based on the array of events given.
 
-# Your algorithm must output the exact integer answer, to full precision.
-# Also, it must correctly handle negative numbers as input.
+# The input array of events will consist of one or more of the following strings:
 
-# HINT I: Can you rearrange the equation fib(n + 2) = fib(n + 1) + fib(n) to
-# find fib(n) if you already know fib(n + 1) and fib(n + 2)? Use this to reason
-# what value fib has to have for negative values.
+# APP_PASSIVE_OPEN, APP_ACTIVE_OPEN, APP_SEND, APP_CLOSE, APP_TIMEOUT, RCV_SYN, RCV_ACK, RCV_SYN_ACK, RCV_FIN, RCV_FIN_ACK
+# The states are as follows and should be returned in all capital letters as shown:
+
+# CLOSED, LISTEN, SYN_SENT, SYN_RCVD, ESTABLISHED, CLOSE_WAIT, LAST_ACK, FIN_WAIT_1, FIN_WAIT_2, CLOSING, TIME_WAIT
+# The input will be an array of events. Your job is to traverse the FSM as determined by the events, and return the proper state as a string, all caps, as shown above.
+
+# If an event is not applicable to the current state, your code will return "ERROR".
+
+# Action of each event upon each state:
+# (the format is INITIAL_STATE: EVENT -> NEW_STATE)
+
+# CLOSED: APP_PASSIVE_OPEN -> LISTEN
+# CLOSED: APP_ACTIVE_OPEN  -> SYN_SENT
+# LISTEN: RCV_SYN          -> SYN_RCVD
+# LISTEN: APP_SEND         -> SYN_SENT
+# LISTEN: APP_CLOSE        -> CLOSED
+# SYN_RCVD: APP_CLOSE      -> FIN_WAIT_1
+# SYN_RCVD: RCV_ACK        -> ESTABLISHED
+# SYN_SENT: RCV_SYN        -> SYN_RCVD
+# SYN_SENT: RCV_SYN_ACK    -> ESTABLISHED
+# SYN_SENT: APP_CLOSE      -> CLOSED
+# ESTABLISHED: APP_CLOSE   -> FIN_WAIT_1
+# ESTABLISHED: RCV_FIN     -> CLOSE_WAIT
+# FIN_WAIT_1: RCV_FIN      -> CLOSING
+# FIN_WAIT_1: RCV_FIN_ACK  -> TIME_WAIT
+# FIN_WAIT_1: RCV_ACK      -> FIN_WAIT_2
+# CLOSING: RCV_ACK         -> TIME_WAIT
+# FIN_WAIT_2: RCV_FIN      -> TIME_WAIT
+# TIME_WAIT: APP_TIMEOUT   -> CLOSED
+# CLOSE_WAIT: APP_CLOSE    -> LAST_ACK
+# LAST_ACK: RCV_ACK        -> CLOSED
+# "EFSM TCP"
+
+# Examples
+# ["APP_PASSIVE_OPEN", "APP_SEND", "RCV_SYN_ACK"] =>  "ESTABLISHED"
+
+# ["APP_ACTIVE_OPEN"] =>  "SYN_SENT"
+
+# ["APP_ACTIVE_OPEN", "RCV_SYN_ACK", "APP_CLOSE", "RCV_FIN_ACK", "RCV_ACK"] =>  "ERROR"
 
 
 class WrongType(Exception):
     pass
 
 # Solution one
-def fib(n):
-    # Handle negative input
-    if n < 0:
-        # Use the recurrence relation fib(n) = (-1)^(n+1) * fib(|n|) for negative n
-        return (-1)**(n+1) * fib(abs(n))
+def traverse_TCP_states(events):
+    state = 'CLOSED'
 
-    # Base cases
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
+    for event in events:
+        if state == 'CLOSED':
+            if event == 'APP_PASSIVE_OPEN':
+                state = 'LISTEN'
+            elif event == 'APP_ACTIVE_OPEN':
+                state = 'SYN_SENT'
+            else:
+                return 'ERROR'
+        elif state == 'LISTEN':
+            if event == 'RCV_SYN':
+                state = 'SYN_RCVD'
+            elif event == 'APP_SEND':
+                state = 'SYN_SENT'
+            elif event == 'APP_CLOSE':
+                state = 'CLOSED'
+            else:
+                return 'ERROR'
+        elif state == 'SYN_RCVD':
+            if event == 'APP_CLOSE':
+                state = 'FIN_WAIT_1'
+            elif event == 'RCV_ACK':
+                state = 'ESTABLISHED'
+            else:
+                return 'ERROR'
+        elif state == 'SYN_SENT':
+            if event == 'RCV_SYN':
+                state = 'SYN_RCVD'
+            elif event == 'RCV_SYN_ACK':
+                state = 'ESTABLISHED'
+            elif event == 'APP_CLOSE':
+                state = 'CLOSED'
+            else:
+                return 'ERROR'
+        elif state == 'ESTABLISHED':
+            if event == 'APP_CLOSE':
+                state = 'FIN_WAIT_1'
+            elif event == 'RCV_FIN':
+                state = 'CLOSE_WAIT'
+            elif event not in ['APP_SEND', 'RCV_ACK', 'RCV_FIN_ACK']:
+                return 'ERROR'
+        elif state == 'FIN_WAIT_1':
+            if event == 'RCV_FIN':
+                state = 'CLOSING'
+            elif event == 'RCV_FIN_ACK':
+                state = 'TIME_WAIT'
+            elif event == 'RCV_ACK':
+                state = 'FIN_WAIT_2'
+            else:
+                return 'ERROR'
+        elif state == 'CLOSING':
+            if event == 'RCV_ACK':
+                state = 'TIME_WAIT'
+            else:
+                return 'ERROR'
+        elif state == 'FIN_WAIT_2':
+            if event == 'RCV_FIN':
+                state = 'TIME_WAIT'
+            elif event == 'RCV_ACK':
+                state = 'ESTABLISHED'
+            else:
+                return 'ERROR'
+        elif state == 'TIME_WAIT':
+            if event == 'APP_TIMEOUT':
+                state = 'CLOSED'
+            else:
+                return 'ERROR'
+        elif state == 'CLOSE_WAIT':
+            if event == 'APP_CLOSE':
+                state = 'LAST_ACK'
+            else:
+                return 'ERROR'
+        elif state == 'LAST_ACK':
+            if event == 'RCV_ACK':
+                state = 'CLOSED'
+            else:
+                return 'ERROR'
+        else:
+            return 'ERROR'
 
-    # Dynamic programming: calculate Fibonacci numbers iteratively
-    fib_n_minus_2 = 0
-    fib_n_minus_1 = 1
-
-    for i in range(2, n + 1):
-        fib_n = fib_n_minus_1 + fib_n_minus_2
-        fib_n_minus_2 = fib_n_minus_1
-        fib_n_minus_1 = fib_n
-
-    return fib_n
+    return state.upper()
 
 
 import cProfile
