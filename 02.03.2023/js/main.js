72cde7739675923058c368c3fa5effe15803c5df
Igor Nadiein <snoozzzy7@gmail.com>
2023-03-02 23:14:24 +0100

    🚧 Added challenge js solution with improved time performance with complexity of O(n)

--- 02.03.2023/js/main.js
+++ 02.03.2023/js/main.js
@@ -51,3 +51,108 @@ export const topThreeWordsOne = (text) => {
     // return the top-3 most occurring words
     return words.slice(0, 3);
 }
+
+// Solution two
+// time complexity of O(n), where n is the number of words in the input text.
+// The idea is to use a hash table to count the frequency of each word,
+//and then use a min heap to keep track of the top-3 most occurring words.
+export const topThreeWordsTwo = (text) => {
+    // create a hash table to count the frequency of each word
+    const counts = {};
+
+    text.toLowerCase().split(/[^\w']+/).forEach(word => {
+        if (word !== '' && !/^'+$/.test(word)) {
+            counts[word] = (counts[word] || 0) + 1;
+        }
+    });
+
+    // create a min heap to keep track of the top-3 most occurring words
+    const heap = new MinHeap();
+
+    Object.keys(counts).forEach(word => {
+        heap.insert({
+            word,
+            count: counts[word]
+        });
+
+        if (heap.size() > 3) {
+            heap.extractMin();
+        }
+    });
+
+    // return the top-3 most occurring words in descending order
+    const result = [];
+
+    while (!heap.isEmpty()) {
+        result.unshift(heap.extractMin().word);
+    }
+
+    return result;
+}
+
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    size() {
+        return this.heap.length;
+    }
+
+    isEmpty() {
+        return this.heap.length === 0;
+    }
+
+    insert(value) {
+        this.heap.push(value);
+        this.bubbleUp(this.heap.length - 1);
+    }
+
+    extractMin() {
+        const min = this.heap[0];
+        const last = this.heap.pop();
+
+        if (!this.isEmpty()) {
+            this.heap[0] = last;
+            this.bubbleDown(0);
+        }
+
+        return min;
+    }
+
+    bubbleUp(index) {
+        const parentIndex = Math.floor((index - 1) / 2);
+
+        if (index > 0 && this.heap[parentIndex].count > this.heap[index].count) {
+            this.swap(index, parentIndex);
+            this.bubbleUp(parentIndex);
+        }
+    }
+
+    bubbleDown(index) {
+        const leftChildIndex = 2 * index + 1;
+        const rightChildIndex = 2 * index + 2;
+        let minIndex = index;
+
+        if (leftChildIndex < this.heap.length &&
+            this.heap[leftChildIndex].count < this.heap[minIndex].count) {
+            minIndex = leftChildIndex;
+        }
+
+        if (rightChildIndex < this.heap.length &&
+            this.heap[rightChildIndex].count < this.heap[minIndex].count) {
+            minIndex = rightChildIndex;
+        }
+
+        if (index !== minIndex) {
+            this.swap(index, minIndex);
+            this.bubbleDown(minIndex);
+        }
+    }
+
+    swap(i, j) {
+        const temp = this.heap[i];
+        this.heap[i] = this.heap[j];
+        this.heap[j] = temp;
+    }
+}
