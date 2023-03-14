117c0ea5917a037550d217b692c81a56be96532b
Igor Nadiein <snoozzzy7@gmail.com>
2023-03-14 23:41:04 +0100

    🎉 Inited challenge js. Added description and solution

--- 09.03.2023/js/main.js
+++ 09.03.2023/js/main.js
@@ -4,7 +4,42 @@
 
 
 /* Solution one */
-// time complexity of the algorithm is O(n)
+// time complexity of the algorithm is O(nlogn), where n is the number of unique lowercase
+// letters in the two input strings combined.
 export const mix = (s1, s2) => {
-    
+    const alphabets = "abcdefghijklmnopqrstuvwxyz";
+    const count1 = {},
+        count2 = {},
+        result = [];
+
+    for (let i = 0; i < alphabets.length; i++) {
+        const letter = alphabets[i];
+        count1[letter] = s1.split(letter).length - 1;
+        count2[letter] = s2.split(letter).length - 1;
+    }
+
+    for (let i = 0; i < alphabets.length; i++) {
+        const letter = alphabets[i];
+        const countInS1 = count1[letter];
+        const countInS2 = count2[letter];
+
+        if (countInS1 > 1 || countInS2 > 1) {
+            if (countInS1 > countInS2) {
+                result.push(`1:${letter.repeat(countInS1)}`);
+            } else if (countInS1 < countInS2) {
+                result.push(`2:${letter.repeat(countInS2)}`);
+            } else {
+                result.push(`=:${letter.repeat(countInS1)}`);
+            }
+        }
+    }
+
+    result.sort((a, b) => {
+        if (b.length !== a.length) {
+            return b.length - a.length;
+        }
+        return a.charCodeAt(0) - b.charCodeAt(0) || a.localeCompare(b);
+    });
+
+    return result.join('/');
 }
